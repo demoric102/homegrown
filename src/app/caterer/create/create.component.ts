d6a2f6eeebed83fde9e51cbc53289d0cfb4d7b82
Adewumi Ademola Eric <demoric102@gmail.com>
2017-09-22 16:41:39 +0100

    schools.json in src/assets

--- src/app/caterer/create/create.component.ts
+++ src/app/caterer/create/create.component.ts
@@ -5,6 +5,8 @@ import { Lga } from "../../models/lga";
 import { CatererService } from "../../services/caterer";
 import { Router } from "@angular/router";
 import { LgaService } from "../../services/lga";
+import { BankService } from "../../services/bank";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'app-create',
@@ -15,24 +17,61 @@ export class CreateComponent implements OnInit {
   private caterer: any = {};
   private lgas$: Observable<Array<Lga>>;
   private banks$: Observable<Array<Bank>>;
+  private progressLoading = false;
+  private alert = {
+    visible: false,
+    status: null,
+    message: ''
+  }
 
   constructor(
     private router: Router,
     private catererService: CatererService,
-    private lgaService: LgaService
+    private lgaService: LgaService,
+    private bankService: BankService
   ) { }
 
   ngOnInit() {
     this.lgas$ = this.lgaService.getAll();
+    this.banks$ = this.bankService.getAll();
   }
   
   onSubmit() {
-    console.log(this.caterer);
-    this.catererService.createCaterer(this.caterer).subscribe((response) => {
-      // this.router.navigate( ['/'] );
-      console.info(response);
-    }, (reason) => {
-      console.warn(reason);
-    })
+    let that = this;
+    this.progressLoading = true;
+    that.alert = {
+      visible: false,
+      status: null,
+      message: ''
+    };
+    this.catererService.createCaterer(this.caterer)
+    .subscribe((data) => {
+      that.caterer = {};
+      that.alert = {
+        visible: true,
+        status: 200,
+        message: ''.concat('A new caterer has been created.')
+      };
+      that.progressLoading = false;
+      console.info(data);
+    }, (error: HttpErrorResponse) => {
+      that.progressLoading = false;
+      if (error.error instanceof Error) {
+        that.alert = {
+          visible: true,
+          status: error.status,
+          message: ''.concat('An error occurred:', error.error.message)
+        };
+      } else {
+        that.alert = {
+          visible: true,
+          status: error.status,
+          message: ''.concat('An error occurred:', error.error.data.invalidAttributes[Object.keys(error.error.data.invalidAttributes)[0]][0].message)
+        };
+      }
+    }, () => {
+      that.progressLoading = false;
+    });
+    
   }
 }
